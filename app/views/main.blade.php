54cda8656aae272b889c9d102061b07db2f4c50d
Bahit Hamid <bahit.hamid@gmail.com>
2014-02-27 01:32:54 +0000

    Fix UI and Boostrap
    Ran 'composer update' to generate twbs into own local vendor file then
    ran:
    'php artisan asset:publish --path="vendor/twbs/bootstrap/dist/"
    bootstrap' to copy distribution copy into public folder.
    
    This created 'packages' folder inside public folder which contains
    'bootstrap'.
    
    Moved custom code with changes made to bootstrap.css > main.css inside
    css folder.
    
    Fixed main.blade.php to show changes and made new navbar

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -9,8 +9,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
        
-        {{ HTML::style('css/bootstrap.css') }}
-        {{ HTML::style('css/bootstrap-responsive.css') }}
+        {{ HTML::style('packages/bootstrap/css/bootstrap.min.css') }}
+        {{ HTML::style('css/main.css') }}
+
 
         <style>
         @section('styles')
@@ -22,21 +23,44 @@
     </head>
 
     <body>
-        <!-- Navbar -->
-        <div class="navbar navbar-inverse navbar-fixed-top">
-            <div class="navbar-brand">
-                <div class="container">
-                    <!-- .btn-navbar is used as the toggle for collapsed navbar content
-                   -->
+        <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          {{ HTML::link('home', 'Faculty Cooperative', array('class' => 'navbar-brand')) }}
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active">{{ HTML::link('home', 'Home') }}</li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li>{{ HTML::link('users/register', 'Register') }}</li>   
+            <li>{{ HTML::link('users/login', 'Login') }}</li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
 
-                    <a class="brand" href="home">Faculty Cooperative</a>
-                    <a class="brand" href="search">Search</a>
-                    <a class="brand" href="#">Link 3</a>
-
-
-                </div>
-            </div>
-        </div> 
 
         <!-- Container -->
         <div class="container">
@@ -47,8 +71,8 @@
         </div>
 
         <!-- Scripts are placed here -->
-        {{ HTML::script('js/jquery.v1.8.3.min.js') }}
-        {{ HTML::script('js/bootstrap/bootstrap.min.js') }}
+        {{ HTML::script('js/jquery.min.js') }}
+        {{ HTML::script('packages/bootstrap/js/bootstrap.min.js') }}
 
     </body>
 </html>

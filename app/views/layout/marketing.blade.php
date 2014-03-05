2c9ac02d4a8c606ae7a6330ba9b7d2f77fc33030
iheras <iheras@essex.ac.uk>
2014-03-05 23:49:46 +0000

    created and added icons for marketing section
    I drew them in Photoshop.

--- app/views/layout/marketing.blade.php
+++ app/views/layout/marketing.blade.php
@@ -8,22 +8,22 @@
       <!-- Three columns of text below the carousel -->
       <div class="row">
         <div class="col-lg-4">
-          <img class="img-circle" data-src="holder.js/140x140/sky" alt="Generic placeholder image">
-          <h2>Heading</h2>
+          <img class="img-circle" ><img src="images/icon_light.png" width="140" height="140" alt="lightbulb" />
+<h2>Have great ideas?</h2>
           <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <p><a class="btn btn-default" href="#" role="button">Sign up now! &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <img class="img-circle" data-src="holder.js/140x140/vine" alt="Generic placeholder image">
-          <h2>Heading</h2>
+          <img class="img-circle" <img src="images/icon_skills.png" width="140" height="140" alt="skills">
+          <h2>Posssess skills?</h2>
           <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <p><a class="btn btn-default" href="#" role="button">Sign up now! &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <img class="img-circle" data-src="holder.js/140x140/lava" alt="Generic placeholder image">
-          <h2>Heading</h2>
+          <img class="img-circle" <img src="images/icon_invest.png" width="140" height="140" alt="investment">
+          <h2>Looking to invest?</h2>
           <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <p><a class="btn btn-default" href="#" role="button">Sign up now! &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
   </div>
